dbc8f15fb04dd0eccb288bdc3fa069464813d099
anisha-01-phoenix <anisha.dat01@gmail.com>
2022-07-27 00:37:31 +0530

    Screen UI

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -11,12 +11,54 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF0A0E21),
-        title: const Text('BMI Calculator'),
-      ),
-      body: Center(
-        child: Text('Main Content'),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF0A0E21),
+          title: const Text('BMI Calculator'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(child: ReusableCard(Color(0xFF1D1E33))),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  final Color colour;
+  ReusableCard(this.colour);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
