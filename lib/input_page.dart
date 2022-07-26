a972f6ea448220dbf8a8040070c3454133ea0d8b
anisha-01-phoenix <anisha.dat01@gmail.com>
2022-07-26 21:33:23 +0530

    Initial commit

---
+++ lib/input_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF0A0E21),
+        title: const Text('BMI Calculator'),
+      ),
+      body: Center(
+        child: Text('Main Content'),
+      ),
+    );
+  }
+}
